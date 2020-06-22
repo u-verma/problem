31a5b7aafb3444f8b0aacc5762bf74666d050e23
u-verma <er.umeshverma@gmail.com>
2020-06-22 23:28:46 +0200

    solution

--- src/main/java/org/easysolution/FindMissingElement.java
+++ src/main/java/org/easysolution/FindMissingElement.java
@@ -1,9 +1,49 @@
 package org.easysolution;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FindMissingElement {
     public static void main(String[] args) {
         int[] arr = {7, 9, 10, 15};
         findMissingElementSequentially(arr);
+
+        int [] unsortedArr = {2, 4, 10, 7};
+        int position = 5;
+
+        int missingElement = findKthMissingElement(unsortedArr, position);
+
+        System.out.println("K th missing element is :" + missingElement);
+    }
+
+    private static int findKthMissingElement(int[] unsortedArr, int position) {
+
+        int min = unsortedArr[0];
+        int max = unsortedArr[0];
+        Set integerSet = new HashSet<Integer>();
+        int missingCounter = 0;
+
+        for (int index = 0; index < unsortedArr.length; index++) {
+
+            if (unsortedArr[index] < min){
+                min = unsortedArr[index];
+            }else if(unsortedArr[index] > max){
+                max = unsortedArr[index];
+            }
+
+            integerSet.add(unsortedArr[index]);
+        }
+
+        for (int counter = min; counter <=max ; counter++) {
+            if(!integerSet.contains(counter)){
+                missingCounter++;
+                if (position == missingCounter){
+                    return counter;
+                }
+            }
+        }
+
+        return -1;
     }
 
     private static void findMissingElementSequentially(int[] arr) {
